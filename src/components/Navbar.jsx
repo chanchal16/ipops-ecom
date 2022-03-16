90612ec39089669e220705b3bab00df2af76be61
chanchal16 <rajputchanchal17@gmail.com>
2022-03-16 19:20:08 +0530

    style - changed class to className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,32 +4,32 @@ import specs from '../assets/spectacle-lenses.svg'
 export default function Navbar() {
   return (
     <div>
-        <header class="navbars">
-            <a href="../index.html" class="menu-icon">
+        <header className="navbars">
+            <a href="../index.html" className="menu-icon">
                 <img src={specs} width="40px" height="40px" alt="logo" />
             </a>
-            <a href="../index.html" class="brand-name h6">
+            <a href="../index.html" className="brand-name h6">
                 ipops
             </a>
             <nav>
                 <ul>
-                    <li class="list-items">
-                        <div class='badge-icon'>
-                            <a href="./wishlist.html"><i class="fas fa-heart fa-lg"></i></a>
-                            <div class='badge dotbadge'>
+                    <li className="list-items">
+                        <div className='badge-icon'>
+                            <a href="./wishlist.html"><i className="fas fa-heart fa-lg"></i></a>
+                            <div className='badge dotbadge'>
                             </div>
                         </div>
                     </li>
-                    <li class="list-items">
-                        <div class='badge-icon'>
-                            <a href="./cart.html"><i class="fas fa-shopping-cart fa-lg"></i></a>
-                            <div class='badge numbadge'>
+                    <li className="list-items">
+                        <div className='badge-icon'>
+                            <a href="./cart.html"><i className="fas fa-shopping-cart fa-lg"></i></a>
+                            <div className='badge numbadge'>
                             <small>1</small>
                             </div>
                         </div>
                     </li>
-                    <li class="list-items">
-                        <i class="far fa-user-circle fa-2x"></i>
+                    <li className="list-items">
+                        <i className="far fa-user-circle fa-2x"></i>
                     </li>
                 </ul>
             </nav>
