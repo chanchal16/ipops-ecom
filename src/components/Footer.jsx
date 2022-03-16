c7926bce298da8a97533fb45e775e5c88378beca
chanchal16 <rajputchanchal17@gmail.com>
2022-03-16 19:19:37 +0530

    style - changed class to className

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-        <footer class="footer">
-            <p class="text-md footer-header">made by &nbsp;<span>Chanchal</span></p>
-            <div class="socials">
-            <a class="links" href="https://github.com/chanchal16" target="_blank">
-                <i class="fab fa-github fa-2x"></i></a>
-            <a class="links" href="https://twitter.com/chanchal16_" target="_blank" >
-                <i class="fab fa-twitter fa-2x"></i></a>
-            <a class="links" href="https://www.linkedin.com/in/rajput07/" target="_blank">
-                <i class="fab fa-linkedin-in fa-2x"></i></a>
+        <footer className="footer">
+            <p className="text-md footer-header">made by &nbsp;<span>Chanchal</span></p>
+            <div className="socials">
+            <a className="links" href="https://github.com/chanchal16" target="_blank">
+                <i className="fab fa-github fa-2x"></i></a>
+            <a className="links" href="https://twitter.com/chanchal16_" target="_blank" >
+                <i className="fab fa-twitter fa-2x"></i></a>
+            <a className="links" href="https://www.linkedin.com/in/rajput07/" target="_blank">
+                <i className="fab fa-linkedin-in fa-2x"></i></a>
             </div>
-            <p class="text-xs footer-header">© 2022 &nbsp;<span>ipops</span></p>
+            <p className="text-xs footer-header">© 2022 &nbsp;<span>ipops</span></p>
         </footer>
     </div>
   )
