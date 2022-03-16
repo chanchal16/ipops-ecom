82b777ff499dbe324a161b5d92d9d7c4e8190304
chanchal16 <rajputchanchal17@gmail.com>
2022-03-16 14:30:00 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <footer class="footer">
+            <p class="text-md footer-header">made by &nbsp;<span>Chanchal</span></p>
+            <div class="socials">
+            <a class="links" href="https://github.com/chanchal16" target="_blank">
+                <i class="fab fa-github fa-2x"></i></a>
+            <a class="links" href="https://twitter.com/chanchal16_" target="_blank" >
+                <i class="fab fa-twitter fa-2x"></i></a>
+            <a class="links" href="https://www.linkedin.com/in/rajput07/" target="_blank">
+                <i class="fab fa-linkedin-in fa-2x"></i></a>
+            </div>
+            <p class="text-xs footer-header">© 2022 &nbsp;<span>ipops</span></p>
+        </footer>
+    </div>
+  )
+}
