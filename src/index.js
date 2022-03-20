b30f08f1fa660ecee4939f90a5818eaff833e697
chanchal16 <rajputchanchal17@gmail.com>
2022-03-20 18:05:15 +0530

    Update - add auth context provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
 import { FilterProvider } from "./contexts/filterContext";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 // Call make Server
 makeServer();
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <FilterProvider>
+        <AuthContextProvider>
         <App />
+        </AuthContextProvider>
       </FilterProvider>
     </Router>
   </React.StrictMode>,
