63f89ef6e8384ce8ea3de1a9825d82744c9c264e
chanchal16 <rajputchanchal17@gmail.com>
2022-03-22 22:45:41 +0530

    Update - add mockman

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import {Navbar,Footer} from "./components/components";
 import {Login,Products,Signup} from "./pages/pages";
+import Mockman from 'mockman-js'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="signup" element={<Signup/> } />
           <Route path="products" element={<Products/>}/>
+          <Route path="mock" element={<Mockman />} />
         </Routes>
       </main>
       <Footer />
