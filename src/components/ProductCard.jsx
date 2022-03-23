bc2a16dbd6da68df234744ca9636cb255c39bddb
chanchal16 <rajputchanchal17@gmail.com>
2022-03-23 12:57:53 +0530

    Update - add wishlist functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,60 +1,58 @@
-import React,{useEffect} from 'react'
-import { useWishlist } from '../contexts/WishlistContext';
-import axios from 'axios'
+import React,{useState,useEffect} from 'react'
+import {useAuth,useWishlist } from '../contexts/contexts';
+import { addToWishlist,removeFromWishlist } from '../services/wishlistServices';
+import { ItemExists } from '../Utils/itemExists';
 
-function ProductCard({product}) {
-    const {state,dispatch} = useWishlist()
-    const addToWishlist = (item)=>{
-        dispatch({type:'ADD_TO_WISHLIST',payload:item})
-    }
+function ProductCard({product}) { 
+    const{token} = useAuth();
+    const {wishlistState,wishlistDispatch} = useWishlist()
+    const [isWishlisted,setIsWishlisted] = useState(false)
 
+    // check for wishlisted items
     useEffect(() => {
-        (async (product) => {
-            try {
-               await axios.post("/api/user/wishlist",{product})
-               .then((res) => {
-                    dispatch({type:'ADD_TO_WISHLIST'})
-                    // addToWishlist(product)
-                console.log('res',res);
-                })
-            }catch(err) { console.log(err)};
-            
-        })();
-    
-      return () => {
-        
-      }
+        setIsWishlisted(ItemExists(wishlistState.wishlist,product._id))   
     }, [])
+    
+    // wishlist handler
+    const wishlistHandler = (product)=>{       
+        if(!isWishlisted){
+            addToWishlist(token,wishlistDispatch,product)
+        }else{
+            removeFromWishlist(token,wishlistDispatch,product._id)
+        }
+        setIsWishlisted(!isWishlisted)
+    }   
+   
   return (
     <div>
         <div class="card" key={product._id}>
-                    <div class="card-media">
-                        <img class="vc-image" 
-                        src={product.img} 
-                        alt="specs" loading="lazy" />
-                        <span class="gray close " onClick={()=>addToWishlist(product)}>
-                            <i class="far fa-heart fa-lg"></i>
-                        </span>
-                        {/* <span class="text-badge">new</span> */}
-                    </div>
-                    <div class="card-content">
-                        <div class="content-title">
-                            <h4>{product.brandname}</h4>
-                            <div class="badge-rating">
-                                <span class="text-xs">{product.rating}</span>
-                                <span class="badge-star"><i class="fas fa-star fa-xs"></i></span>
-                            </div>  
-                        </div>
-                        <div class="desc">
-                            <p class="desc-title">{product.categoryName}</p>
-                            <p><strong>₹{product.price} </strong> <span class="strike-text gray">Rs.999
-                                </span> <span class="secondary">20% off</span></p>
-                        </div>
-                        <div class="action-btns">
-                            <button class="btn"><i class="fas fa-shopping-cart"></i> Add to cart</button>
-                        </div>
-                    </div>
+            <div class="card-media">
+                <img class="vc-image" 
+                src={product.img} 
+                alt="specs" loading="lazy" />
+                <span class="gray close " onClick={()=>wishlistHandler(product)}>
+                    {isWishlisted ?<i className="fas fa-heart fa-lg"></i>
+                    : <i className="far fa-heart fa-lg"></i>}
+                </span>
+            </div>
+            <div class="card-content">
+                <div class="content-title">
+                    <h4>{product.brandname}</h4>
+                    <div class="badge-rating">
+                        <span class="text-xs">{product.rating}</span>
+                        <span class="badge-star"><i class="fas fa-star fa-xs"></i></span>
+                    </div>  
                 </div>
+                <div class="desc">
+                    <p class="desc-title">{product.categoryName}</p>
+                    <p><strong>₹{product.price} </strong> <span class="strike-text gray">Rs.999
+                        </span> <span class="secondary">20% off</span></p>
+                </div>
+                <div class="action-btns">
+                    <button class="btn"><i class="fas fa-shopping-cart"></i> Add to cart</button>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
