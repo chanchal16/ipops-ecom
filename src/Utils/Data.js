dfe8295063520d96bb42552e4333ae8aa49a285f
chanchal16 <rajputchanchal17@gmail.com>
2022-03-26 20:11:14 +0530

    Create data for homepage

---
+++ src/Utils/Data.js
@@ -0,0 +1,58 @@
+const trends = [
+    {
+        id:1,
+        type:'Round',
+        img:'https://static1.lenskart.com/media/desktop/img/Sep21/image179.png'
+    },
+    {
+        id:2,
+        type:'Cat-eye',
+        img:'https://static1.lenskart.com/media/desktop/img/Sep21/cateeye.jpg'
+    },
+    {
+        id:3,
+        type:'Transparent',
+        img:'https://static1.lenskart.com/media/desktop/img/Sep21/trans.jpg'
+    },
+    {
+        id:4,
+        type:'Aviator',
+        img:'https://cdn.eyemyeye.com/desktop/home-page/hm-pilot-eyeglassses.jpg'
+    },
+    {
+        id:5,
+        type:'Blend Edit',
+        img:'https://static1.lenskart.com/media/desktop/img/Sep21/blend.jpg'
+    }
+
+]
+
+// services data
+const services = [
+    {
+        id:1,
+        service:'100% MADE IN INDIA',
+        img:'https://cdn.eyemyeye.com/mobile/mobile-site-content/icons/ftr_made_in_india.svg'
+    },
+    {
+        id:2,
+        service:'HIGH QUALITY PRODUCTS',
+        img:'https://cdn.eyemyeye.com/desktop/images/icon/ftr_best_quality.svg'
+    },
+    {
+        id:3,
+        service:'FAST TRACK DELIVERY',
+        img:'https://cdn.eyemyeye.com/desktop/images/icon/ftr_faster_delivery.svg'
+    },
+    {
+        id:4,
+        service:'14-DAY EASY RETURNS',
+        img:'https://cdn.eyemyeye.com/desktop/images/icon/ftr_easy_returns.svg'
+    },
+    {
+        id:5,
+        service:'100% SECURE PAYMENT',
+        img:'https://cdn.eyemyeye.com/desktop/images/icon/ftr_secure_payment.svg'
+    }
+]
+export {trends,services}
