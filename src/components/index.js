65953eb4beb2b8dc7c3c218e27603c40011b0231
chanchal16 <rajputchanchal17@gmail.com>
2022-04-06 12:54:34 +0530

    chore - add address component

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {Navbar} from './Navbar';
+export {Sidebar} from './Sidebar';
+export {ProductCard} from './ProductCard';
+export {AddressCard} from './AddressCard';
+export {Spinner} from './Spinner'
+export {Footer} from './Footer'
