51123161aed0d05038aa01fc7fa7f18ee2e9ba72
chanchal16 <rajputchanchal17@gmail.com>
2022-04-07 11:57:48 +0530

    chore - add address form

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export {Navbar} from './Navbar';
 export {Sidebar} from './Sidebar';
 export {ProductCard} from './ProductCard';
+export {AddressForm} from './AddressForm';
 export {AddressCard} from './AddressCard';
 export {Spinner} from './Spinner'
 export {Footer} from './Footer'
