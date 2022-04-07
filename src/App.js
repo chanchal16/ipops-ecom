f82976417e81a08cdfdf526944ac82e8cf821ebb
chanchal16 <rajputchanchal17@gmail.com>
2022-04-07 23:51:17 +0530

    chore - add page not found

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import {Navbar,Footer} from "./components/main-component";
-import {Login,Products,Signup,Wishlist,Cart,Home} from "./pages/main";
-import Mockman from 'mockman-js'
+import {Login,Products,Signup,Wishlist,Cart,Home,PageNotFound} from "./pages/main";
+import Mockman from 'mockman-js';
+import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <ToastContainer autoClose={1000} theme="dark" />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -16,6 +19,7 @@ function App() {
           <Route path="products" element={<Products/>}/>
           <Route path="wishlist" element={<Wishlist/>}/>
           <Route path="cart" element={<Cart/>}/> 
+          <Route path="*" element={<PageNotFound/>}/>
           <Route path="mock" element={<Mockman />} />
         </Routes>
       </main>
