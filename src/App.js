7c7a39253c5cdfebb21ca23b3ddb241a5184acda
chanchal16 <rajputchanchal17@gmail.com>
2022-05-09 18:33:58 +0530

    refactor -moved routes to separate file

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import {Navbar,Footer,AddressForm,AddressList} from "./components";
-import {Login,Products,Signup,Wishlist,Cart,Home,PageNotFound,OrderSummary} from "./pages/main";
-import Mockman from 'mockman-js';
+import { useRoutes } from "react-router-dom";
+import {Navbar,Footer,AddressForm} from "./components";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ROUTES } from "./routes";
 
 function App() {
+  const routeElements = useRoutes(ROUTES)
   return (
     <div className="App">
       <Navbar/>
       <ToastContainer autoClose={1000} theme="dark" />
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="signup" element={<Signup/> } />
-          <Route path="products" element={<Products/>}/>
-          <Route path="wishlist" element={<Wishlist/>}/>
-          <Route path="cart" element={<Cart/>}/> 
-          <Route path="address" element={<AddressList/>}/>
-          <Route path="order-summary" element={<OrderSummary/>}/>
-          <Route path="*" element={<PageNotFound/>}/>
-          <Route path="mock" element={<Mockman />} />
-        </Routes>
+        {routeElements}
         <AddressForm/> 
       </main>
       <Footer />
     </div>
   );
 }
-
 export default App;
