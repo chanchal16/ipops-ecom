6dc1678bad14ff2d9d77257020329d63277805d0
chanchal16 <rajputchanchal17@gmail.com>
2022-07-01 13:31:39 +0530

    chore - change id to _id

--- src/Utils/itemExists.js
+++ src/Utils/itemExists.js
@@ -1,4 +1,4 @@
-export const ItemExists = (array, id) => {
+export const ItemExists = (array, _id) => {
     // returns true or false
-    return !!array.find((item) => item.id === id);
+    return !!array.find((item) => item._id === _id);
 };
